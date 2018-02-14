841d558bcdab72234c367696b0252af5a15e3f66
Chris Wohlers <chriswohlers@gmail.com>
2018-02-13 22:56:29 -0800

    Add basic signup and login frontend.

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -5,7 +5,17 @@ class Login extends Component {
     return (
       <div className="login">
         <h3>Login</h3>
-        <p>Login form goes here.</p>
+        <form method="post" action="http://localhost:3000/login">
+          <div className="input">
+            <label>Username: </label>
+            <input name="username" id="username" type="text"/>
+          </div>
+          <div className="input">
+            <label>Password: </label>
+            <input name="password" id="password" type="password"/>
+          </div>
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
     );
   }
