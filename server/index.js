d0bc0a0e214e04f06a016d6e2db83eef320c8b1e
AlexWellsHS <alex@headspace.com>
2018-02-14 11:46:44 -0800

    update endpoints

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
-const bodyParser = require('body-parser');
+// const Courses = require('../services/courses');
+// const Enrollments = require('../services/enrollments');
+// const Auth = require('../services/enrollments');
+// const Users = require('../services/users');
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
@@ -10,29 +14,57 @@ const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 // courses
 app.get('/courses', wrap(async (req, res) => {
-  const courses = await db.getCourses();
+  const courses = await Courses.get();
   res.send(courses);
 }));
 
 app.get('/courses/:courseId', wrap(async (req, res) => {
-  const course = await db.getCourse(req.params.courseId);
+  const course = await Courses.get(req.params.courseId);
   res.send(course);
-}))
+}));
 
 app.post('/courses', wrap(async (req, res) => {
-  const courseId = await db.addCourse(req.body);
+  const courseId = await Courses.create(req.body);
   res.send(courseId);
 }));
 
-// user-courses
-app.get('/user-courses/:userId', wrap(async (req, res) => {
-  const userCourses = await db.getUserCourses(req.params.userId)
-  res.send(userCourses);
+// enrollments
+app.get('/enrollments/:userId', wrap(async (req, res) => {
+  const enrollments = await Enrollments.get(req.params.userId);
+  res.send(enrollments);
 }));
 
-app.post('/user-courses', wrap(async (req, res) => {
-  const userCourseId = await db.addUserCourse(req.body)
+app.post('/enrollments', wrap(async (req, res) => {
+  const userCourseId = await Enrollments.create(req.body);
   res.send(userCourseId);
 }));
+
+app.put('/enrollments', wrap(async (req, res) => {
+  const enrollment = await Enrollments.update(req.body);
+  res.send(enrollment);
+
+  const { courseId } = req.body;
+  Courses.updateRating(courseId);
+}));
+
+// users
+app.get('/users/:userId', wrap(async (req, res) => {
+  const user = await Users.get(req.params.userId);
+  res.send(user);
+}));
+
+app.post('/users', wrap(async (req, res) => {
+  const user = await Users.create(req.body);
+  res.send(user);
+}));
+
+// todo
+// auth - use session tokens
+// app.post('/login', wrap(async (req, res) => {
+//   res.send();
+// }));
+
+// app.delete('/')
+// log user out - destroy token
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
