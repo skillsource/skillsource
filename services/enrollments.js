6641184db334dde2d625712c90acc67855422dc1
Spencer <spencer0cornelia@gmail.com>
2018-02-14 20:29:19 -0800

    added services and skeleton starter functions for users enrollments and courses

---
+++ services/enrollments.js
@@ -0,0 +1,18 @@
+const Course = require('../db/models/course.js');
+const User = require('../db/models/user.js');
+
+const get = function(userID) {
+  // Course.findAll({})
+}
+
+const create = function() {
+
+}
+
+const update = function() {
+
+}
+
+module.exports.get = get;
+module.exports.create = create;
+module.exports.update = update;
