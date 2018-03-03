dc1c3e98789359e407d1176fad72a59b99d6614c
Fat Pikachu <fatpikachu@Fats-MacBook-Pro.local>
2018-03-03 15:18:59 -0800

    reactstars dont work

--- client/src/components/courseHeader.jsx
+++ client/src/components/courseHeader.jsx
@@ -11,13 +11,16 @@ class CourseHeader extends React.Component {
   }
 
   render(){
+    const rating = this.props.enrolled
+    ? (<ReactStars value={this.props.course.rating} size={20} edit={true} />)
+    : (<p>hi hi hi </p>)
     return (
       <div className="course-header">
         <div id="course-name">
           <h3>{this.props.course.name}</h3>
         </div>
         <div id="course-rating">
-          <ReactStars value={this.props.course.rating} size={20} edit={false}/>
+          {rating}
         </div>
         <div id="course-enroll">
           <Enroll handleEnrollment={this.props.handleEnrollment} enrolled={this.props.enrolled} loggedIn={this.props.loggedIn} />
