96735f23c6f21f2c207223cdf601526602a96091
Chris Wohlers <chriswohlers@gmail.com>
2018-03-05 20:59:55 -0800

    Fix error rendering steps descriptions. Add some styling to steps component. Delete rating.jsx file.

--- db/sampleData/sampleData.js
+++ db/sampleData/sampleData.js
@@ -46,63 +46,63 @@ const steps = [
   {
     ordinalNumber: 0,
     name: 'Learn the basics',
-    description: 'Code Academy does a great job teaching you the basics. Start here.',
+    text: 'Code Academy does a great job teaching you the basics. Start here.',
     url: 'https://www.codecademy.com/en/tracks/javascript',
     courseId: 1
   },
   {
     ordinalNumber: 1,
     name: 'Build your first app with Gordon Zhu',
-    description: 'Now that you\'ve learned the basics, let\'s get a taste of web enginering. Gordon\'s course does a great job walking you through building a ToDo list app' ,
+    text: 'Now that you\'ve learned the basics, let\'s get a taste of web enginering. Gordon\'s course does a great job walking you through building a ToDo list app' ,
     url: 'https://watchandcode.com/p/practical-javascript',
     courseId: 1
   },
   {
     ordinalNumber: 2,
     name: 'Download React',
-    description: 'Congrats, you\'re a senior software engineer now. Go download React and change the world',
+    text: 'Congrats, you\'re a senior software engineer now. Go download React and change the world',
     url: 'https://reactjs.org/',
     courseId: 1
   },
   {
     ordinalNumber: 0,
     name: 'Work on your kick',
-    description: 'Kicking is important when you swim',
+    text: 'Kicking is important when you swim',
     url: 'bla',
     courseId: 2
   },
   {
     ordinalNumber: 1,
     name: 'Learn how to breath efficiently',
-    description: 'Breathing is important when you swim. Watch this great youtube vordinalNumbereo.',
+    text: 'Breathing is important when you swim. Watch this great youtube vordinalNumbereo.',
     url: 'http://youtube.com/tuningukesforbeginners/',
     courseId: 2
   },
   {
     ordinalNumber: 2,
     name: 'Master the flip turn',
-    description: 'The faster you turn, the faster you go.',
+    text: 'The faster you turn, the faster you go.',
     url: 'http://youtube.com/YourFirstUkeSong/',
     courseId: 2
   },
   {
     ordinalNumber: 0,
     name: 'Pick your instrument',
-    description: 'Choosing the right ukelele can be tough. Here are a few things to consordinalNumberer',
+    text: 'Choosing the right ukelele can be tough. Here are a few things to consordinalNumberer',
     url: 'https://www.ukuleletricks.com/5-best-ukuleles-to-buy-for-beginners/',
     courseId: 3
   },
   {
     ordinalNumber: 1,
     name: 'Get your uke tuned',
-    description: 'For this course we\'re going to use G, C, E, A tuning. Watch this youtube vordinalNumber for step by step instructions',
+    text: 'For this course we\'re going to use G, C, E, A tuning. Watch this youtube vordinalNumber for step by step instructions',
     url: 'https://takelessons.com/blog/how-to-tune-a-ukulele-beginners-z10',
     courseId: 3
   },
   {
     ordinalNumber: 2,
     name: 'Practice your first song',
-    description: 'I know, you just want to play \'Somewhere Over The Rainbow\' but let\'s start with something a little simpler to build up our skills',
+    text: 'I know, you just want to play \'Somewhere Over The Rainbow\' but let\'s start with something a little simpler to build up our skills',
     url: 'https://www.youtube.com/watch?v=C7cPKXfI4CM',
     courseId: 3
   }
