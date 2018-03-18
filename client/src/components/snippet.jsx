9e1d2fd31952719467962a7aa69fc765e3182d1d
AlexWellsHS <alex@headspace.com>
2018-03-18 01:14:01 -0700

    scrollable browse component with preview

--- client/src/components/snippet.jsx
+++ client/src/components/snippet.jsx
@@ -4,6 +4,21 @@ import moment from 'moment';
 
 
 class Snippet extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onMouseEnter = () => {
+    if (this.props.onMouseEnter) {
+      this.props.onMouseEnter(this.props.data.steps);
+    }
+  }
+
+  onMouseLeave = () => {
+    if (this.props.onMouseLeave) {
+      this.props.onMouseLeave();
+    }
+  }
 
   render() {
     const { id, name, rating, description, steps } = this.props.data;
@@ -15,35 +30,35 @@ class Snippet extends Component {
     const time = moment.duration(totalMinutes, 'minutes').humanize();
 
     return (
-      <a href={url}>
-      <div className="snippet">
-        <div className="snippet-name">
-          <h4>{name}</h4>
+      <a href={url} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        <div className="snippet">
+          <div className="snippet-name">
+            <h4>{name}</h4>
+          </div>
+          <div className="snippet-time">
+            <h4>Estimated Time: {time}</h4>
+          </div>
+          <div className="snippet-rating">
+            <StarRatingComponent
+              name="rating"
+              starCount={5}
+              value={rating}
+              editing={false}
+            />
+          </div>
+          {
+              Number.isInteger(this.props.progress)
+              ? this.props.progress === 100
+                ? <p className='progress'>Completed!</p>
+                : <div className='progress'><p>In progress: </p>
+                <p>{this.props.progress}% complete. </p>
+                </div>
+              : <p></p>
+          }
+          <div className="snippet-description">
+          <p>{description}</p>
+          </div>
         </div>
-        <div className="snippet-time">
-          <h4>Estimated Time: {time}</h4>
-        </div>
-        <div className="snippet-rating">
-          <StarRatingComponent
-            name="rating"
-            starCount={5}
-            value={rating}
-            editing={false}
-          />
-        </div>
-        {
-            Number.isInteger(this.props.progress)
-            ? this.props.progress === 100
-              ? <p className='progress'>Completed!</p>
-              : <div className='progress'><p>In progress: </p>
-              <p>{this.props.progress}% complete. </p>
-              </div>
-            : <p></p>
-        }
-        <div className="snippet-description">
-        <p>{description}</p>
-        </div>
-      </div>
       </a>
     );
   }
