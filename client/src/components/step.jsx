f066907530fd0ad173e1988d5dded2c3186005a0
Chris Poole <poolecs@gmail.com>
2018-03-21 14:46:01 -0700

    Re-sync commit

--- client/src/components/step.jsx
+++ client/src/components/step.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import MediaUploaded from "./mediaUploaded.jsx";
 import ApiService from "../services/ApiService.js";
 import urlmodule from 'url';
 import moment from 'moment';
